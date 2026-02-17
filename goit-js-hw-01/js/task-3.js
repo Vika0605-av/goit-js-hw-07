2d0d9c0a7b3aafaea4b55c69f8ffb550e318c656
Vika0605-av <polina031014@gmail.com>
2026-02-17 16:53:25 +0100

    Add files via upload
    correct

---
+++ goit-js-hw-01/js/task-3.js
@@ -0,0 +1,6 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+input.addEventListener('input', () => {
+    const value = input.value.trim();
+    output.textContent = value === '' ? 'Anonymous' : value;
+});
