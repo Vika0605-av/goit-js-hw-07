30170adda0dab84007ec84fa580f1c27b526de8d
Vika0605-av <polina031014@gmail.com>
2026-02-17 15:53:23 +0100

    Add files via upload
    correct

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,10 @@
-function makeTransaction(quantity,pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const ul = document.guerySelector('.categories');
+const items = ul.guerySelectorAll('.li');
+console.log(items.length);
+const categories = document.querySelectorAll('.categories .item');
+categories.forEach(category => {
+    const title = category.querySelector('h2').textContent;
+    const elements = category.querySelectorAll('li').length;
+    console.log('Category:  ${title}');
+    console.log('Elements: ${elements}');
+});
